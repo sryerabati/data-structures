4ec706245094183fb5afffa64c9d51a7118b18fd
sryerabati <sryerabati@stu.naperville203.org>
2023-10-09 22:04:38 -0500

    idk

---
+++ Chapter 15 Activities/FloodFill/src/Pair.java
@@ -0,0 +1,14 @@
+public class Pair {
+    private int num1, num2;
+
+    public Pair(int n1,int n2){
+        num1 = n1;
+        num2 = n2;
+    }
+    public int col(){
+        return num2;
+    }
+    public int row(){
+        return num1;
+    }
+}
